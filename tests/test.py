042e660fed6364cb420e694498012ea8e901ac28
Leo Basov <basov.leo@gmail.com>
2018-12-02 15:19:34 +0100

    Added tests.

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../.')
+
+from discrete_pack.trees.octree import Octree
+
+def test():
+	tree = Octree()
+
+	print(60*'=')
+	print('> Discrete pack tree test')
+	print(60*'=')
+
+test()
